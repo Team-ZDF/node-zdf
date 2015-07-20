6fb4718fae9969f44f01d8275c2708387ad01e3c
Tyler Benfield <rtbenfield@gmail.com>
2015-07-19 21:38:20 -0400

    Made packager stream compatible and better error handling

--- examples/packager.js
+++ examples/packager.js
@@ -1,8 +1,29 @@
 var fs = require('fs');
+var fstream = require('fstream');
+var path = require('path');
 var zdf = require('../zdf.js');
 
-var directory = __dirname + '/how-to-spy/';
-console.log(directory);
-var destination = fs.createWriteStream(__dirname + '/how-to-spy.zdf');
+var sourceDirectory = path.join(__dirname, 'how-to-spy');
 
-zdf.package(directory).pipe(destination);
+var destinationStream = fs.createWriteStream(path.join(__dirname, 'how-to-spy.zdf'))
+  .on('error', function(err) {
+    console.error('Destination error occurred: ', err);
+  })
+  .on('finish', function() {
+    console.log('Write Complete');
+  });
+
+zdf.package(sourceDirectory)
+  .on('error', function(err) {
+    console.error('ZDF error occurred: ', err);
+  })
+  .pipe(destinationStream);
+
+// var packageStream = zdf.createPackageStream();
+//
+// fstream.Reader({
+//     path: sourceDirectory,
+//     type: 'Directory'
+//   })
+//   .pipe(packageStream)
+//   .pipe(destinationStream);
--- zdf.js
+++ zdf.js
@@ -1,6 +1,7 @@
 var packageJson = require('./package.json');
 
 module.exports = {
+  createPackageStream: require('./lib/package-stream.js'),
   read: require('./lib/reader.js'),
   package: require('./lib/packager.js'),
   version: 'node-zdf version ' + packageJson.version
